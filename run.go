562e65c9355cb63b2fec3363eda37fb096a0b9c5
Andy <4ndygeiss@gmail.com>
2024-08-18 11:39:39 +0200

    refactor: rename cfg to stateData

--- run.go
+++ run.go
@@ -3,11 +3,11 @@ package fsm
 import "context"
 
 // StateFn is a recursive function which handles a specific state.
-type StateFn[T any] func(ctx context.Context, cfg T) (stateFn StateFn[T])
+type StateFn[T any] func(ctx context.Context, stateData T) (stateFn StateFn[T])
 
 // Run just run until the state goes nil.
-func Run[T any](startFn StateFn[T], ctx context.Context, cfg T) {
+func Run[T any](startFn StateFn[T], ctx context.Context, stateData T) {
 	for state := startFn; state != nil; {
-		state = state(ctx, cfg)
+		state = state(ctx, stateData)
 	}
 }
--- run_test.go
+++ run_test.go
@@ -14,74 +14,74 @@ func setup() (ctx context.Context, cancel context.CancelFunc) {
 
 func TestRun_Given_0_Should_Return_0_After_StateFn(t *testing.T) {
 	// Arrange
-	type config struct{ state int }
+	type stateData struct{ state int }
 	ctx, cancel := setup()
 	defer cancel()
-	cfg := config{state: 0}
-	stateFn := func(ctx context.Context, cfg config) fsm.StateFn[config] {
+	data := stateData{state: 0}
+	stateFn := func(ctx context.Context, data stateData) fsm.StateFn[stateData] {
 		return nil
 	}
 
 	// Act
-	go fsm.Run(stateFn, ctx, cfg)
+	go fsm.Run(stateFn, ctx, data)
 
 	// Assert
 	select {
 	case <-ctx.Done():
 	}
 
-	if cfg.state != 0 {
-		t.Errorf("State should be 0, but got %d", cfg.state)
+	if data.state != 0 {
+		t.Errorf("State should be 0, but got %d", data.state)
 	}
 }
 
 func TestRun_Given_0_Should_Return_1_After_StateFn(t *testing.T) {
 	// Arrange
-	type config struct{ state int }
+	type stateData struct{ state int }
 	ctx, cancel := setup()
 	defer cancel()
-	cfg := &config{state: 0}
-	stateFn := func(ctx context.Context, cfg *config) fsm.StateFn[*config] {
-		cfg.state = 1
+	data := &stateData{state: 0}
+	stateFn := func(ctx context.Context, data *stateData) fsm.StateFn[*stateData] {
+		data.state = 1
 		return nil
 	}
 
 	// Act
-	go fsm.Run(stateFn, ctx, cfg)
+	go fsm.Run(stateFn, ctx, data)
 
 	// Assert
 	select {
 	case <-ctx.Done():
 	}
 
-	if cfg.state != 1 {
-		t.Errorf("State should be 1, but got %d", cfg.state)
+	if data.state != 1 {
+		t.Errorf("State should be 1, but got %d", data.state)
 	}
 }
 
 func TestRun_Given_0_Should_Return_2_After_Two_StateFn(t *testing.T) {
 	// Arrange
-	type config struct{ state int }
+	type stateData struct{ state int }
 	ctx, cancel := setup()
 	defer cancel()
-	cfg := &config{state: 0}
-	stateFnB := func(ctx context.Context, cfg *config) fsm.StateFn[*config] {
-		cfg.state++
+	data := &stateData{state: 0}
+	stateFnB := func(ctx context.Context, data *stateData) fsm.StateFn[*stateData] {
+		data.state++
 		return nil
 	}
-	stateFnA := func(ctx context.Context, cfg *config) fsm.StateFn[*config] {
-		cfg.state++
-		return stateFnB(ctx, cfg)
+	stateFnA := func(ctx context.Context, data *stateData) fsm.StateFn[*stateData] {
+		data.state++
+		return stateFnB(ctx, data)
 	}
 	// Act
-	go fsm.Run(stateFnA, ctx, cfg)
+	go fsm.Run(stateFnA, ctx, data)
 
 	// Assert
 	select {
 	case <-ctx.Done():
 	}
 
-	if cfg.state != 2 {
-		t.Errorf("State should be 2, but got %d", cfg.state)
+	if data.state != 2 {
+		t.Errorf("State should be 2, but got %d", data.state)
 	}
 }
